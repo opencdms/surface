bdf577b0180f5a83abf034d92f2294e5bb72a1d9
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-11-27 12:54:24 +0100

    install certbot in nginx container

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,8 +1,17 @@
-/init-letsencrypt.sh
 export DOLLAR="$"
-envsubst < /etc/nginx/conf.d/nginx.conf.template >| /etc/nginx/conf.d/nginx.conf
+cat /etc/nginx-conf-template/nginx.conf.http.template >| /etc/nginx/conf.d/nginx.conf
 /wait-for-it.sh -t 30 api:8000;
-nginx -g 'daemon off;';
+echo $HOST_FQDN
+if [ -z ${HOST_FQDN+x} ];
+then
+  nginx;
+else
+  nginx;
+  certbot certonly --webroot --webroot-path /var/www/certbot/ -d ${HOST_FQDN} --non-interactive --agree-tos -m info@${HOST_FQDN}
+  envsubst < /etc/nginx-conf-template/nginx.conf.https.template >| /etc/nginx/conf.d/nginx.conf
+  nginx -s stop;
+  nginx;
+fi
 while :;
 do
   sleep 6h;
