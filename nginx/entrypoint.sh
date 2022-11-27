e4bc7d4cd3f69e50b221c15e66f59ba61fcf17b7
Faysal Ishtiaq <f.i.rabby@gmail.com>
2022-11-27 13:45:06 +0100

    add different templates for local and http only deployment

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,11 +1,11 @@
 export DOLLAR="$"
-cat /etc/nginx-conf-template/nginx.conf.http.template >| /etc/nginx/conf.d/nginx.conf
 /wait-for-it.sh -t 30 api:8000;
-echo $HOST_FQDN
 if [ -z "$HOST_FQDN" ];
 then
+  cat /etc/nginx-conf-template/nginx.conf.local.template >| /etc/nginx/conf.d/nginx.conf
   nginx;
 else
+  cat /etc/nginx-conf-template/nginx.conf.http.template >| /etc/nginx/conf.d/nginx.conf
   nginx;
   certbot certonly --webroot --webroot-path /var/www/certbot/ -d ${HOST_FQDN} --non-interactive --agree-tos -m info@${HOST_FQDN}
   envsubst < /etc/nginx-conf-template/nginx.conf.https.template >| /etc/nginx/conf.d/nginx.conf
