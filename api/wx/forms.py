42ec47d79cbcc5641cb4c33c5162974baa9db99d
jeremiahhammond14@gmail.com <jeremiahhammond14@gmail.com>
2024-09-04 19:59:53 +0000

    update wigos input

--- api/wx/forms.py
+++ api/wx/forms.py
@@ -13,6 +13,7 @@ class FTPServerForm(forms.ModelForm):
 
 
 class StationForm(forms.ModelForm):
+    # set utc_offset label
     utc_offset_minutes = forms.IntegerField(
         label='UTC Offset (min)', 
         initial=settings.TIMEZONE_OFFSET
@@ -32,6 +33,12 @@ class StationForm(forms.ModelForm):
 
     region = forms.ChoiceField(choices=region_options)
 
+    # configure wigos section options
+    wigos_part_1 = forms.IntegerField(initial='0', disabled=True, required=False, label='WIGOS ID Series')
+    wigos_part_2 = forms.CharField(max_length=16, required=False, label='Issuer of identifier')
+    wigos_part_3 = forms.IntegerField(min_value=0, max_value=65534, required=False, label='Issue Number')
+    wigos_part_4 = forms.CharField(max_length=16, required=False, label='Local Identifier')
+
     class Meta:
         model = Station
         fields = '__all__'
@@ -39,6 +46,24 @@ class StationForm(forms.ModelForm):
             'wigos': 'WIGOS ID',
             'code': 'Station ID',
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        # Get the individual parts of the WIGOS ID
+        wigos_part_1 = cleaned_data.get('wigos_part_1')
+        wigos_part_2 = cleaned_data.get('wigos_part_2')
+        wigos_part_3 = cleaned_data.get('wigos_part_3')
+        wigos_part_4 = cleaned_data.get('wigos_part_4')
+
+        # Combine the parts into a single string separated by '-'
+        if wigos_part_1 and wigos_part_2 and wigos_part_3 and wigos_part_4:
+            wigos_combined = f"{wigos_part_1}-{wigos_part_2}-{wigos_part_3}-{wigos_part_4}"
+
+            # Set the combined value to the 'wigos' field
+            cleaned_data['wigos'] = wigos_combined
+
+        return cleaned_data
 
 
 class WxGroupPermissionForm(forms.ModelForm):
